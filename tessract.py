cce35a3e1c88883303e673fb6ad281eeb591f4e6
Олег Моисеев <olegmoiseev2698@gmail.com>
2022-12-23 12:30:56 +0300

    Initial commit

---
+++ tessract.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+
+# C:\Program Files\Tesseract-OCR
+
+
+def thresh_callback(val):
+    threshold = val
+
+    canny_output = cv2.Canny(src_gray, threshold, threshold * 2)
+
+    # contours, _ = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(canny_output, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    contours_poly = [None] * len(contours)
+    boundRect = [None] * len(contours)
+    for i, c in enumerate(contours):
+        contours_poly[i] = cv2.approxPolyDP(c, 3, True)
+        boundRect[i] = cv2.boundingRect(contours_poly[i])
+
+    drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
+
+    print(len(contours))
+    additional_bound = 5
+    bounds = []
+    for i in range(len(contours)):
+        color = (0, 0, 255)
+        cv2.drawContours(drawing, contours_poly, i, color)
+        bounds.append((int(boundRect[i][0] - additional_bound), int(boundRect[i][1] - additional_bound),  # left, up
+                      int(boundRect[i][0] + boundRect[i][2]) + additional_bound, int(boundRect[i][1] + boundRect[i][3]) + additional_bound))
+
+        cv2.rectangle(drawing, (int(boundRect[i][0] - additional_bound), int(boundRect[i][1] - additional_bound)),  # left, up
+                      (int(boundRect[i][0] + boundRect[i][2]) + additional_bound, int(boundRect[i][1] + boundRect[i][3]) + additional_bound), color, 2)
+
+    print(bounds)
+    print(bounds[0][0], bounds[0][1], bounds[0][2], bounds[0][3])  # x1, y1, x2, y2
+    roi = src_gray[bounds[0][1]:bounds[0][3], bounds[0][0]:bounds[0][2]]  # y1:y2, x1:x2
+    cv2.imwrite("roi1.jpg", roi)
+    cv2.imshow('Contours', drawing)
+
+
+src = cv2.imread('data_num_recog/img_100.jpg')
+# src = cv2.resize(src, (600, 360))
+
+# Convert image to gray and blur it
+src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+src_gray = cv2.blur(src_gray, (3, 3))
+
+
+thresh = 100 # initial threshold
+thresh_callback(thresh)
+
+# Create Window
+source_window = 'Source'
+cv2.namedWindow(source_window)
+cv2.imshow(source_window, src_gray)
+
+cv2.waitKey()
---
+++ torchnn.py
@@ -0,0 +1,73 @@
+# Import dependencies
+import torch
+from PIL import Image
+from torch import nn, save, load
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Grayscale, Resize
+from torchvision.transforms.functional import pil_to_tensor
+
+
+# Get data 
+train = datasets.MNIST(root="data_num_recog", download=False, train=True, transform=ToTensor())
+dataset = DataLoader(train, 32)
+
+
+# 1,28,28 - classes 0-9
+
+# Image Classifier Neural Network
+class ImageClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 32, (3, 3)),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, (3, 3)),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, (3, 3)),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(64 * (28 - 6) * (28 - 6), 10)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+# Instance of the neural network, loss, optimizer
+clf = ImageClassifier().to('cuda')
+opt = Adam(clf.parameters(), lr=1e-3)
+loss_fn = nn.CrossEntropyLoss()
+
+# Training flow 
+if __name__ == "__main__":
+    for epoch in range(10):  # train for 10 epochs
+        for batch in dataset:
+            X, y = batch
+            X, y = X.to('cuda'), y.to('cuda')
+            yhat = clf(X)
+            loss = loss_fn(yhat, y)
+
+            # Apply backprop
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+
+        print(f"Epoch:{epoch} loss is {loss.item()}")
+        if loss.item() < 1e-5:
+            break
+
+    with open('model_state_cuda.pt', 'wb') as f:
+        save(clf.state_dict(), f)
+
+    # with open('model_state_cuda.pt', 'rb') as f:
+    #     clf.load_state_dict(load(f))
+    #
+    # img = Image.open('data_num_recog/img_6.jpg')
+    # img = Grayscale(1)(img)
+    # img = Resize((28, 28))(img)
+    # # img_gray = Grayscale(1)(img)
+    # # img_tensor = pil_to_tensor(img_gray).unsqueeze(0)
+    # img_tensor = ToTensor()(img).unsqueeze(0).to('cuda')
+    # print(torch.argmax(clf(img_tensor)))
